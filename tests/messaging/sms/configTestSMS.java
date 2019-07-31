964b09161f6d78e50aa64bbd4af5c7ed989e0fd7
viram-jain <viramjain9742@gmail.com>
2019-07-31 11:08:25 +0530

    Final Commit

--- tests/messaging/sms/configTestSMS.java
+++ tests/messaging/sms/configTestSMS.java
@@ -6,8 +6,8 @@
 package tests.messaging.sms;
 
 public class configTestSMS {
-    public static long number = 919742052352L;
-    public static String message = "Hello_there";
+    public static long number = 91L;
+    public static String message = "message";
     public static String scheduleDate = "16-08-2019 07:00 PM",scheduleFormat = "dd-MM-yyyy hh:mm a";
     public static String modifyDate = "16-08-2019 10:00 PM",modifyFormat = "dd-MM-yyyy hh:mm a";
     public static String fromDate="12-06-2019",toDate="11-07-2019",creditFormat="dd-MM-yyyy";
